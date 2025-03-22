9d6a58ebf0336bdd1efadd8de2d7d6b7839c8402
Naotaka Hasegawa <webcordnn@gmail.com>
2025-03-22 17:18:15 +0900

    RSSフィードの作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bright-blancmange-d42712.netlify.app/', // ここにNetlifyのサイトURLを入力
+});
